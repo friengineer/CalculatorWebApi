d670b21662978f37a4b323e88dc7a0f7657f309c
friengineer <174277232+friengineer@users.noreply.github.com>
2024-07-21 20:30:12 +0100

    Add unit tests (#11)
    Added units tests for all actions

---
+++ CalculatorWebApi.Tests/CalculatorControllerTests.cs
@@ -0,0 +1,82 @@
+using CalculatorWebApi.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System.Text.Json.Nodes;
+
+namespace CalculatorWebApi.Tests
+{
+    public class CalculatorControllerTests
+    {
+        private static readonly JsonObject _requestPayload = new() { ["firstNumber"] = 10, ["secondNumber"] = 5 };
+        private ILogger<CalculatorController> _logger;
+        private CalculatorController _controller;
+
+        [SetUp]
+        public void Setup()
+        {
+            _logger = Mock.Of<ILogger<CalculatorController>>();
+            _controller = new CalculatorController(_logger);
+        }
+
+        [Test]
+        public void Add_ReturnsOkResponse_WithCorrectResult()
+        {
+            // Arrange
+            var expectedResponse = new OkObjectResult(new { Result = 15 });
+
+            // Act
+            var response = _controller.Add(_requestPayload);
+            var okResponse = (OkObjectResult)response;
+
+            // Assert
+            Assert.That(response, Is.TypeOf<OkObjectResult>());
+            Assert.That(okResponse.Value!.ToString(), Is.EqualTo(expectedResponse.Value!.ToString()));
+        }
+
+        [Test]
+        public void Subtract_ReturnsOkResponse_WithCorrectResult()
+        {
+            // Arrange
+            var expectedResponse = new OkObjectResult(new { Result = 5 });
+
+            // Act
+            var response = _controller.Subtract(_requestPayload);
+            var okResponse = (OkObjectResult)response;
+
+            // Assert
+            Assert.That(response, Is.TypeOf<OkObjectResult>());
+            Assert.That(okResponse.Value!.ToString(), Is.EqualTo(expectedResponse.Value!.ToString()));
+        }
+
+        [Test]
+        public void Multiply_ReturnsOkResponse_WithCorrectResult()
+        {
+            // Arrange
+            var expectedResponse = new OkObjectResult(new { Result = 50 });
+
+            // Act
+            var response = _controller.Multiply(_requestPayload);
+            var okResponse = (OkObjectResult)response;
+
+            // Assert
+            Assert.That(response, Is.TypeOf<OkObjectResult>());
+            Assert.That(okResponse.Value!.ToString(), Is.EqualTo(expectedResponse.Value!.ToString()));
+        }
+
+        [Test]
+        public void Divide_ReturnsOkResponse_WithCorrectResult()
+        {
+            // Arrange
+            var expectedResponse = new OkObjectResult(new { Result = 2 });
+
+            // Act
+            var response = _controller.Divide(_requestPayload);
+            var okResponse = (OkObjectResult)response;
+
+            // Assert
+            Assert.That(response, Is.TypeOf<OkObjectResult>());
+            Assert.That(okResponse.Value!.ToString(), Is.EqualTo(expectedResponse.Value!.ToString()));
+        }
+    }
+}
